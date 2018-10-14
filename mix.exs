ba20a73940bb87d4ffbd81143d380d6c33cff307
Humberto Rodriguez Avila <rhumbert@vub.ac.be>
2018-10-14 17:43:27 +0200

    Add MIVB monitor as dependency

--- mix.exs
+++ mix.exs
@@ -38,6 +38,7 @@ defmodule BxlLive.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
+      {:mivbm, git: "https://github.com/rhumbertgz/mivbm.git"},
     ]
   end
 end
