49c39f7724e71f9621f97c1f83e7cc6b5bd21b73
Humberto Rodriguez Avila <rhumbert@vub.ac.be>
2018-10-16 14:44:08 +0200

    Update prod.exs configuration

--- config/prod.exs
+++ config/prod.exs
@@ -1,23 +1,14 @@
 use Mix.Config
 
-# For development, we disable any cache and enable
-# debugging and code reloading.
-#
-# The watchers configuration can be used to run external
-# watchers to your application. For example, we use it
-# with brunch.io to recompile .js and .css sources.
 config :bxl_live, BxlLiveWeb.Endpoint,
+  load_from_system_env: true,
   http: [port: 8000],
   debug_errors: false,
   code_reloader: false,
-  code_reloading: false,
   check_origin: false
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 
 # Do not include metadata nor timestamps in development logs
-config :logger, :console, format: "[$level] $message\n"
-
-# Set a higher stacktrace during development. Avoid configuring such
-# in production as building large stacktraces may be expensive.
-config :phoenix, :stacktrace_depth, 20
+config :logger, level: :info
 
