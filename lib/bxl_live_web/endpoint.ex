1022a12810ec2527c8d86a7591c778dd940b0d72
Humberto Rodriguez Avila <rhumbert@vub.ac.be>
2018-10-16 14:19:59 +0200

    fixed code reloader bug

--- lib/bxl_live_web/endpoint.ex
+++ lib/bxl_live_web/endpoint.ex
@@ -13,7 +13,7 @@ defmodule BxlLiveWeb.Endpoint do
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
-  if code_reloading? do
+  if Application.get_env(:bxl_live, BxlLiveWeb.Endpoint)[:code_reloader] do
     socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
     plug Phoenix.LiveReloader
     plug Phoenix.CodeReloader
