5f55acca673fa4e69af2067c370f30cf79220cfe
Humberto Rodriguez Avila <rhumbert@vub.ac.be>
2018-11-13 23:38:25 +0100

    Replace the cowboy dependency by plug_cowboy

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule BxlLive.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"},
+      {:plug_cowboy, "~> 1.0"},
       {:mivbm, git: "git@github.com:rhumbertgz/mivbm.git", tag: "0.1.2"},
     ]
   end
